610d25c1e7f17641f2420bbfbbac5c2e65b8de0e
Jasper <jgordon3@bowdoin.edu>
2023-02-02 16:24:48 -0500

    Demo of Calculator
    Still working on this

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -19,155 +19,190 @@ export const Calculator = () => {
     flour: 100,
   });
 
-  const [weights, setWeights] = React.useState({
-    flour: 0,
-    water: 0,
-    salt: 0,
-    yeast: 0,
-    oil: 0,
-  });
+    const [weights, setWeights] = React.useState({
+      flour: 0,
+      water: 0,
+      salt: 0,
+      yeast: 0,
+      oil: 0,
+    });
+
+  // let flour = 0;
+  // let salt = 0;
+  // let yeast = 0;
+  // let oil = 0;
+  // let water = 0;
 
   const calculateRecipe = () => {
     console.log(
       values.flour + values.water + values.salt + values.yeast + values.oil
     );
     let totalPercent =
-      (values.flour + values.water + values.salt + values.yeast + values.oil) *
+      (values.flour +
+        values.water +
+        values.salt +
+        values.yeast +
+        values.oil) *
       0.01;
     console.log("Percent " + totalPercent);
+    console.log("Type of water: " + typeof values.water);
     let totalWeight = values.size * values.pizzas;
     console.log("Weight" + totalWeight);
-    const flourW = totalWeight / totalPercent
+    const flourW = totalWeight / totalPercent;
     console.log("Flour" + flourW);
-    setWeights({flour: flourW});
-    setWeights({water: weights.flour * values.water});
-    setWeights({salt: weights.flour * values.salt});
-    setWeights({yeast: weights.flour * values.yeast});
-    setWeights({oil: weights.flour * values.oil});
-    console.log("flour weight: " +  weights.flour)
+    flour = totalWeight / totalPercent;
+    water = flour * values.water;
+    salt = flour * values.salt;
+    yeast = flour * values.yeast;
+    oil = flour * values.oil;
+
+    // setWeights({flour: flourW});
+    // setWeights({water: weights.flour * values.water});
+    // setWeights({salt: weights.flour * values.salt});
+    // setWeights({yeast: weights.flour * values.yeast});
+    // setWeights({oil: weights.flour * values.oil});
   };
 
   const handleChange = (prop) => (event) => {
-    setValues({ ...values, [prop]: event.target.value });
+    //Converting input to a float
+    if (event.target.value != "") {
+      setValues({ ...values, [prop]: parseFloat(event.target.value) });
+      //BUG: Causes error if non num so dealing with empty stirng but other issues remain
+      //Couple this solution with understnading of error validation for text field
+    } else {
+      setValues({ ...values, [prop]: event.target.value });
+    }
   };
 
-  const updateWeights = (prop) => (event) => {
-    setWeights({ ...weights, [prop]: event.target.value });
-    console.log("I was called");
-  };
+  //   const updateWeights = (prop) => (event) => {
+  //     setWeights({ ...weights, [prop]: event.target.value });
+  //     console.log("I was called");
+  //   };
 
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexWrap: "wrap",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <FormControl sx={{ m: 1, width: "9ch" }} variant="outlined">
-        <OutlinedInput
-          id="outlined-adornment-water"
-          value={values.water}
-          onChange={handleChange("water")}
-          endAdornment={<InputAdornment position="end">%</InputAdornment>}
-          error={values.water !== "" && isNaN(values.water)}
-          aria-describedby="outlined-water-helper-text"
-          inputProps={{
-            "aria-label": "water",
-          }}
-        />
-        <FormHelperText id="outlined-water-helper-text">Water</FormHelperText>
-      </FormControl>
-
-      <FormControl sx={{ m: 1, width: "9ch" }} variant="outlined">
-        <OutlinedInput
-          id="outlined-adornment-salt"
-          value={values.salt}
-          onChange={handleChange("salt")}
-          endAdornment={<InputAdornment position="end">%</InputAdornment>}
-          error={values.salt !== "" && isNaN(values.salt)}
-          aria-describedby="outlined-salt-helper-text"
-          inputProps={{
-            "aria-label": "salt",
-          }}
-        />
-        <FormHelperText id="outlined-salt-helper-text">Salt</FormHelperText>
-      </FormControl>
-
-      <FormControl sx={{ m: 1, width: "9ch" }} variant="outlined">
-        <OutlinedInput
-          id="outlined-adornment-yeast"
-          value={values.yeast}
-          onChange={handleChange("yeast")}
-          endAdornment={<InputAdornment position="end">%</InputAdornment>}
-          error={values.yeast !== "" && isNaN(values.yeast)}
-          aria-describedby="outlined-yeast-helper-text"
-          inputProps={{
-            "aria-label": "yeast",
-          }}
-        />
-        <FormHelperText id="outlined-yeast-helper-text">Yeast</FormHelperText>
-      </FormControl>
-
-      <FormControl sx={{ m: 1, width: "9ch" }} variant="outlined">
-        <OutlinedInput
-          id="outlined-adornment-oil"
-          value={values.oil}
-          onChange={handleChange("oil")}
-          endAdornment={<InputAdornment position="end">%</InputAdornment>}
-          error={values.oil !== "" && isNaN(values.oil)}
-          aria-describedby="outlined-oil-helper-text"
-          inputProps={{
-            "aria-label": "oil",
-          }}
-        />
-        <FormHelperText id="outlined-oil-helper-text">Oil</FormHelperText>
-      </FormControl>
-
-      <FormControl sx={{ m: 1, width: "9ch" }} variant="outlined">
-        <OutlinedInput
-          id="outlined-adornment-pizzas"
-          value={values.pizzas}
-          onChange={handleChange("pizzas")}
-          error={values.pizzas !== "" && isNaN(values.pizzas)}
-          aria-describedby="outlined-pizzas-helper-text"
-          inputProps={{
-            "aria-label": "pizzas",
-          }}
-        />
-        <FormHelperText id="outlined-oil-helper-text">
-          Number of Pizzas
-        </FormHelperText>
-      </FormControl>
-
-      <FormControl sx={{ m: 1, width: "9ch" }} variant="outlined">
-        <OutlinedInput
-          id="outlined-adornment-size"
-          value={values.size}
-          onChange={handleChange("size")}
-          endAdornment={<InputAdornment position="end">g</InputAdornment>}
-          error={values.size !== "" && isNaN(values.size)}
-          aria-describedby="outlined-size-helper-text"
-          inputProps={{
-            "aria-label": "size",
-          }}
-        />
-        <FormHelperText id="outlined-oil-helper-text">
-          Size of Pizza
-        </FormHelperText>
-      </FormControl>
-
-      <Button
-        onClick={() => {
-          calculateRecipe();
-          console.log("This is flour weight: " + weights.flour);
+    <>
+      <Box
+        sx={{
+          display: "flex",
+          flexWrap: "wrap",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
         }}
-        variant="contained"
       >
-        Calculate
-      </Button>
-    </Box>
+        <FormControl sx={{ m: 1, width: "9ch" }} variant="outlined">
+          <OutlinedInput
+            id="outlined-adornment-water"
+            value={values.water}
+            onChange={handleChange("water")}
+            endAdornment={<InputAdornment position="end">%</InputAdornment>}
+            error={values.water !== "" && isNaN(values.water)}
+            aria-describedby="outlined-water-helper-text"
+            inputProps={{
+              "aria-label": "water",
+            }}
+          />
+          <FormHelperText id="outlined-water-helper-text">Water</FormHelperText>
+        </FormControl>
+
+        <FormControl sx={{ m: 1, width: "9ch" }} variant="outlined">
+          <OutlinedInput
+            id="outlined-adornment-salt"
+            value={values.salt}
+            onChange={handleChange("salt")}
+            endAdornment={<InputAdornment position="end">%</InputAdornment>}
+            error={values.salt !== "" && isNaN(values.salt)}
+            aria-describedby="outlined-salt-helper-text"
+            inputProps={{
+              "aria-label": "salt",
+            }}
+          />
+          <FormHelperText id="outlined-salt-helper-text">Salt</FormHelperText>
+        </FormControl>
+
+        <FormControl sx={{ m: 1, width: "9ch" }} variant="outlined">
+          <OutlinedInput
+            id="outlined-adornment-yeast"
+            value={values.yeast}
+            onChange={handleChange("yeast")}
+            endAdornment={<InputAdornment position="end">%</InputAdornment>}
+            error={values.yeast !== "" && isNaN(values.yeast)}
+            aria-describedby="outlined-yeast-helper-text"
+            inputProps={{
+              "aria-label": "yeast",
+            }}
+          />
+          <FormHelperText id="outlined-yeast-helper-text">Yeast</FormHelperText>
+        </FormControl>
+
+        <FormControl sx={{ m: 1, width: "9ch" }} variant="outlined">
+          <OutlinedInput
+            id="outlined-adornment-oil"
+            value={values.oil}
+            onChange={handleChange("oil")}
+            endAdornment={<InputAdornment position="end">%</InputAdornment>}
+            error={values.oil !== "" && isNaN(values.oil)}
+            aria-describedby="outlined-oil-helper-text"
+            inputProps={{
+              "aria-label": "oil",
+            }}
+          />
+          <FormHelperText id="outlined-oil-helper-text">Oil</FormHelperText>
+        </FormControl>
+
+        <FormControl sx={{ m: 1, width: "9ch" }} variant="outlined">
+          <OutlinedInput
+            id="outlined-adornment-pizzas"
+            value={values.pizzas}
+            onChange={handleChange("pizzas")}
+            error={values.pizzas !== "" && isNaN(values.pizzas)}
+            aria-describedby="outlined-pizzas-helper-text"
+            inputProps={{
+              "aria-label": "pizzas",
+            }}
+          />
+          <FormHelperText id="outlined-oil-helper-text">
+            Number of Pizzas
+          </FormHelperText>
+        </FormControl>
+
+        <FormControl sx={{ m: 1, width: "9ch" }} variant="outlined">
+          <OutlinedInput
+            id="outlined-adornment-size"
+            value={values.size}
+            onChange={handleChange("size")}
+            endAdornment={<InputAdornment position="end">g</InputAdornment>}
+            error={values.size !== "" && isNaN(values.size)}
+            aria-describedby="outlined-size-helper-text"
+            inputProps={{
+              "aria-label": "size",
+            }}
+          />
+          <FormHelperText id="outlined-oil-helper-text">
+            Size of Pizza
+          </FormHelperText>
+        </FormControl>
+
+        <Button
+          onClick={() => {
+            calculateRecipe();
+            console.log("This is flour weight: " + flour);
+            
+          }}
+          variant="contained"
+        >
+          Calculate
+        </Button>
+      </Box>
+
+      <div>
+          <h2> Recipe</h2>
+          <li> Flour: {flour}g</li>
+          <li> Water: {water}g</li>
+          <li> Salt: {salt}g</li>
+          <li> Yeast: {yeast}g</li>
+          <li> Oil: {oil}g</li>
+      </div>
+    </>
   );
 };
